a155025aeedcc2394819fcad9aeb487ca180ee95
oleg <olegvraby@gmail.com>
2023-09-08 17:21:19 +0200

    removed arrows Carousel

--- src/components/product/ProductSectionCart.tsx
+++ src/components/product/ProductSectionCart.tsx
@@ -14,8 +14,6 @@ import Faq from '../Faq';
 import Carousel from 'react-multi-carousel';
 import 'react-multi-carousel/lib/styles.css';
 
-import { CustomRightArrow, CustomLeftArrow } from '../arrowsCarousel';
-
 interface ProductSectionCartProps {
   product: { [key: string]: string | string[] };
 }
@@ -122,6 +120,51 @@ const ProductSectionCart = ({ product }: ProductSectionCartProps) => {
       .slice(index)
       .concat(initialSlide.slice(0, index));
     setInitialMobailImg(Array.isArray(newArray) ? newArray : []);
+  };
+
+  const CustomRightArrow = ({ ...rest }) => {
+    const { onClick } = rest;
+    return (
+      <button
+        className="absolute right-0 rounded-full transition linear  duration-250 hover:bg-black hover:text-white "
+        onClick={() => onClick()}
+      >
+        <svg
+          width="50"
+          height="50"
+          viewBox="0 0 16 16"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+          className="flex justify-center items-center"
+        >
+          <path
+            d="M6 3L11 8L6 13"
+            stroke="CurrentColor"
+            strokeWidth="1"
+            className="block"
+          ></path>
+        </svg>
+      </button>
+    );
+  };
+  const CustomLeftArrow = ({ ...rest }) => {
+    const { onClick } = rest;
+    return (
+      <button
+        className=" absolute left-0 rounded-full  rotate-180 transition linear  duration-250 hover:bg-black hover:text-white "
+        onClick={() => onClick()}
+      >
+        <svg
+          width="50"
+          height="50"
+          viewBox="0 0 16 16"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path d="M6 3L11 8L6 13" stroke="CurrentColor" strokeWidth="1"></path>
+        </svg>
+      </button>
+    );
   };
 
   return (
