253272bd008accdea1df8b2c221b23d78eaa2416
oleg <olegvraby@gmail.com>
2023-10-11 16:25:08 +0200

    added filter

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,76 +5,78 @@ import { useSelector } from 'react-redux';
 import { RootState } from '../store/index';
 import '../index.css';
 import {
-  Nav,
-  BgClose,
-  BgCloseSearch,
-  BgCloseCart,
-  SideBarCompany,
-  SidebarAboutUs,
-  SidebarSustainAbility,
-  SidebarDisignSpace,
-  SideBarMagazine,
-  SidebarStore,
-  SidebarProduct,
-  SidebarHptSelected,
-  SidebarReStore,
-  SideBarSearch,
-  SideBarCart,
-  ToggleNav,
-  NavMobail,
+   Nav,
+   BgClose,
+   BgCloseSearch,
+   BgCloseCart,
+   SideBarCompany,
+   SidebarAboutUs,
+   SidebarSustainAbility,
+   SidebarDisignSpace,
+   SideBarMagazine,
+   SidebarStore,
+   SidebarProduct,
+   SidebarHptSelected,
+   SidebarReStore,
+   SideBarSearch,
+   SideBarCart,
+   ToggleNav,
+   NavMobail,
 } from './index';
 
 const Header = () => {
-  const dispatch = useDispatch();
-  const toggleSlice = useSelector((state: RootState) => state.acardion.toggle);
-  const widthSlice = useSelector((state: RootState) => state.size.widthNav);
-  const [InititalWidth, setInititalWidth] = useState(false);
-  const {
-    toggleStore,
-    sidebarStoreProduct,
-    sidebarStoreHptSelected,
-    sidebarStoreReStore,
-    toggleMagazine,
-    toggleCompany,
-    sidebarleCompanyAboutUs,
-    sidebarleCompanySustainAbility,
-    sidebarleCompanyDisignSpace,
-    toggleSearch,
-    toggleCart,
-  } = useSelector((state: RootState) => state.sideBar);
+   const dispatch = useDispatch();
+   const toggleSlice = useSelector((state: RootState) => state.acardion.toggle);
+   const widthSlice = useSelector((state: RootState) => state.size.widthNav);
+   const [InititalWidth, setInititalWidth] = useState(false);
+   const {
+      toggleStore,
+      sidebarStoreProduct,
+      sidebarStoreHptSelected,
+      sidebarStoreReStore,
+      toggleMagazine,
+      toggleCompany,
+      sidebarleCompanyAboutUs,
+      sidebarleCompanySustainAbility,
+      sidebarleCompanyDisignSpace,
+      toggleSearch,
+      toggleCart,
+   } = useSelector((state: RootState) => state.sideBar);
 
-  useEffect(() => {
-    if (+widthSlice < 768) {
-      setInititalWidth(true);
-    } else {
-      setInititalWidth(false);
-      dispatch(hiddeAction());
-    }
-  }, [widthSlice, dispatch, toggleSlice]);
+   useEffect(() => {
+      if (+widthSlice < 768) {
+         setInititalWidth(true);
+      } else {
+         setInititalWidth(false);
+         dispatch(hiddeAction());
+      }
+   }, [widthSlice, dispatch, toggleSlice]);
 
-  return (
-    <header>
-      {InititalWidth && <ToggleNav />}
-      {toggleCart && <BgCloseCart />}
-      {toggleSearch && <BgCloseSearch />}
-      {(toggleStore || toggleMagazine || toggleCompany) && <BgClose />}
-      {toggleStore && <SidebarStore />}
-      {toggleStore && sidebarStoreProduct && <SidebarProduct />}
-      {toggleStore && sidebarStoreHptSelected && <SidebarHptSelected />}
-      {toggleStore && sidebarStoreReStore && <SidebarReStore />}
-      {toggleMagazine && <SideBarMagazine />}
-      {toggleCompany && <SideBarCompany />}
-      {toggleCompany && sidebarleCompanyAboutUs && <SidebarAboutUs />}
-      {toggleCompany && sidebarleCompanySustainAbility && (
-        <SidebarSustainAbility />
-      )}
-      {toggleCompany && sidebarleCompanyDisignSpace && <SidebarDisignSpace />}
-      <Nav />
-      {InititalWidth && toggleSlice && <NavMobail />}
-      {toggleCart && <SideBarCart />}
-      {toggleSearch && <SideBarSearch />}
-    </header>
-  );
+   return (
+      <header>
+         {InititalWidth && <ToggleNav />}
+         {toggleCart && <BgCloseCart />}
+         {toggleSearch && <BgCloseSearch />}
+         {(toggleStore || toggleMagazine || toggleCompany) && <BgClose />}
+         {toggleStore && <SidebarStore />}
+         {toggleStore && sidebarStoreProduct && <SidebarProduct />}
+         {toggleStore && sidebarStoreHptSelected && <SidebarHptSelected />}
+         {toggleStore && sidebarStoreReStore && <SidebarReStore />}
+         {toggleMagazine && <SideBarMagazine />}
+         {toggleCompany && <SideBarCompany />}
+         {toggleCompany && sidebarleCompanyAboutUs && <SidebarAboutUs />}
+         {toggleCompany && sidebarleCompanySustainAbility && (
+            <SidebarSustainAbility />
+         )}
+         {toggleCompany && sidebarleCompanyDisignSpace && (
+            <SidebarDisignSpace />
+         )}
+         <Nav />
+         {InititalWidth && toggleSlice && <NavMobail />}
+         {toggleCart && <SideBarCart />}
+         {toggleSearch && <SideBarSearch />}
+      </header>
+   );
 };
 
 export default Header;
--- src/components/Navbar/Cart/SideBarSearch.tsx
+++ src/components/Navbar/Cart/SideBarSearch.tsx
@@ -3,17 +3,43 @@ import { toggleSearch } from '../../../store/redusers/sideBarReduser';
 
 import { Card, Typography, Input } from '@material-tailwind/react';
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
+import { productCard } from '../../../dammyDB/dammyProducts';
+import { Link } from 'react-router-dom';
+import { nanoid } from '@reduxjs/toolkit';
+import { useEffect, useState } from 'react';
 
 export default function SideBarSearch() {
    const dispatch = useDispatch();
+
+   const [filter, setFilter] = useState([
+      { mainImg1: '', mainImg2: '', title: '', type: '', price: '', src: '' },
+   ]);
+   const [inputValue, setInputValue] = useState('');
+
+   useEffect(() => {
+      const filter =
+         inputValue.length > 0
+            ? productCard.filter(
+                 (item) =>
+                    item.title.includes(inputValue.toLowerCase()) ||
+                    item.type.includes(inputValue.toLowerCase())
+              )
+            : [];
+
+      setFilter(filter);
+   }, [inputValue]);
 
    const clickHandler = () => {
       dispatch(toggleSearch());
    };
 
+   const changeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+      setInputValue(e.target.value);
+   };
+
    return (
       <Card
-         className={`z-40 fixed w-[100%] rounded-none right-0 h-full md:w-[30%]`}
+         className={`z-40 fixed w-[100%] rounded-none right-0 h-full xl:w-[30%] lg:w-[40%] md:w-[50%] overflow-y-auto`}
       >
          <div className="mb-2 flex justify-between items-center gap-4 p-4">
             <Typography variant="h5" color="blue-gray">
@@ -40,9 +66,39 @@ export default function SideBarSearch() {
                className="text-black"
                color="black"
                icon={<MagnifyingGlassIcon className="h-5 w-5 cursor-pointer" />}
+               onChange={changeHandler}
                label="Search"
+               value={inputValue}
             />
          </div>
+
+         {inputValue.length > 0 && (
+            <div className="flex flex-col px-[10%]">
+               {filter.map((product) => (
+                  <Link
+                     key={nanoid()}
+                     to={`${product.src}`}
+                     className="flex gap-5 mt-5"
+                  >
+                     <div className="basis-[60%]">
+                        <img
+                           src={require(`../../../dammyDB/${product.mainImg2}`)}
+                           alt={`${product.mainImg2}`}
+                           className="w-full h-full object-cover brightness-95"
+                        />
+                     </div>
+                     <div className="w-full">
+                        <Typography className="text-lg text-black font-bold">
+                           {product.title}
+                        </Typography>
+                        <Typography className="underline">
+                           €{product.price},00
+                        </Typography>
+                     </div>
+                  </Link>
+               ))}
+            </div>
+         )}
       </Card>
    );
 }
