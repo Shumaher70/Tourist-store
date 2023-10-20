d5eac313ec84457b4dd69d734c0965d1216e7395
oleg <olegvraby@gmail.com>
2023-10-20 14:28:10 +0200

    change orderSummery component

--- src/components/Navbar/Cart/shoppingCart/components/orderSummery/OrderSummery.tsx
+++ src/components/Navbar/Cart/shoppingCart/components/orderSummery/OrderSummery.tsx
@@ -10,7 +10,25 @@ import Subtotal from './components/Subtotal';
 import Shipping from './components/Shipping';
 import TotalPrice from './components/TotalPrice';
 
-const OrderSummery = () => {
+interface OrderSummeryProps {
+   className?: string;
+   removeShowHiderOrder?: boolean;
+   removeCart?: boolean;
+   removeDiscount?: boolean;
+   removeSubtotal?: boolean;
+   removeShipping?: boolean;
+   removeTotalPrice?: boolean;
+}
+
+const OrderSummery = ({
+   className,
+   removeShowHiderOrder,
+   removeCart,
+   removeDiscount,
+   removeSubtotal,
+   removeShipping,
+   removeTotalPrice,
+}: OrderSummeryProps) => {
    const productSlice = useSelector((state: RootState) => state.cart.products);
    const [show, setShow] = useState(false);
    const clickHandler = (e: boolean) => {
@@ -18,29 +36,33 @@ const OrderSummery = () => {
    };
 
    return (
-      <div className="w-full bg-black/10 p-5">
+      <div className={`w-full px-6 py-5 ${className}`}>
          <AnimateHeight
             id="example-panel"
             duration={500}
-            className="max-w-[600px] m-auto "
+            className="max-w-[600px] m-auto"
             height={show ? 'auto' : 30}
          >
-            <ShowHideOrder clickHandler={clickHandler} />
-            <div className="flex flex-col gap-3 border-t-[1px]  mt-5 border-black/20 py-5">
-               {productSlice.map((product) => (
-                  <SingleCart
-                     key={nanoid()}
-                     totalPrice={product.totalPriceProduct}
-                     img={product.mainImg}
-                     title={product.title}
-                     quantity={product.quantity}
-                  />
-               ))}
-            </div>
-            <Discount />
-            <Subtotal className="mt-5" />
-            <Shipping />
-            <TotalPrice />
+            {!removeShowHiderOrder && (
+               <ShowHideOrder clickHandler={clickHandler} />
+            )}
+            {!removeCart && (
+               <div className="flex flex-col gap-3 border-t-[1px] mt-5 border-black/20 py-5 ">
+                  {productSlice.map((product) => (
+                     <SingleCart
+                        key={nanoid()}
+                        totalPrice={product.totalPriceProduct}
+                        img={product.mainImg}
+                        title={product.title}
+                        quantity={product.quantity}
+                     />
+                  ))}
+               </div>
+            )}
+            {!removeDiscount && <Discount />}
+            {!removeSubtotal && <Subtotal className="mt-5" />}
+            {!removeShipping && <Shipping />}
+            {!removeTotalPrice && <TotalPrice />}
          </AnimateHeight>
       </div>
    );
