748b5cfd7505518af943c76a6d007bfe567ee01d
oleg <olegvraby@gmail.com>
2023-10-25 09:18:17 +0200

    optimize VideoSection

--- src/components/product/VideoSection.tsx
+++ src/components/product/VideoSection.tsx
@@ -3,22 +3,23 @@ import { Typography } from '@material-tailwind/react';
 import { nanoid } from '@reduxjs/toolkit';
 import PlayButton from '../../pages/img/PlayButton';
 import CustomButtonExit from '../CustomButtonExit';
-
 import { useInView } from 'react-intersection-observer';
 
-interface videoSectionProps {
+interface VideoSectionProps {
    product: {
       videoImg: string;
       video: string;
       description: string;
    }[];
-
    videoSectionhandler: (element: boolean) => void;
 }
 
-const VideoSection = ({ product, videoSectionhandler }: videoSectionProps) => {
-   const [openVideo, setOpenVideo] = useState<boolean>(false);
-   const [indexVideo, setindexVideo] = useState<number>(0);
+const VideoSection: React.FC<VideoSectionProps> = ({
+   product,
+   videoSectionhandler,
+}) => {
+   const [openVideo, setOpenVideo] = useState(false);
+   const [indexVideo, setIndexVideo] = useState(0);
    const [isVisible, setIsVisible] = useState(false);
    const { ref, inView } = useInView({ threshold: 0.5 });
 
@@ -31,21 +32,12 @@ const VideoSection = ({ product, videoSectionhandler }: videoSectionProps) => {
       videoSectionhandler(isVisible);
    }, [inView, isVisible, videoSectionhandler]);
 
-   const clickHandler = (event?: React.MouseEvent) => {
-      const index =
-         event?.currentTarget.parentElement?.getAttribute('data-index');
-
-      if (index) setindexVideo(parseInt(index));
-
+   const clickHandler = (event: React.MouseEvent, index: number) => {
+      event.preventDefault();
+      setIndexVideo(index);
       setOpenVideo(true);
-      if (event?.currentTarget) {
-         setOpenVideo(true);
-      } else {
-         setOpenVideo(false);
-      }
    };
 
-   const video = product;
    return (
       <section id="videos" className="relative wrapper">
          <Typography
@@ -55,36 +47,30 @@ const VideoSection = ({ product, videoSectionhandler }: videoSectionProps) => {
             watch videos
          </Typography>
          <div className="mt-10 lg:columns-3 sm:columns-2 columns-1">
-            {video &&
-               video.map((item, index) => {
-                  return (
-                     <div
-                        key={nanoid()}
-                        data-index={index}
-                        className="flex flex-col break-inside-avoid-column"
-                     >
-                        <div
-                           onClick={clickHandler}
-                           className="relative cursor-pointer"
-                        >
-                           <img
-                              src={require(`../../dammyDB/${item.videoImg}`)}
-                              alt={item.videoImg}
-                              className="w-full 2xl:h-[300px] xl:h-[250px] lg:h-[200px] sm:h-[200px] object-contein z-50"
-                           />
-                           <div className="absolute top-2/4 left-2/4 -translate-x-2/4 -translate-y-2/4 ">
-                              <PlayButton />
-                           </div>
-                        </div>
-
-                        <Typography className="uppercase font-normal lg:text-1xl border-b-[1px] border-black mt-2 mb-5">
-                           {item.description}
-                        </Typography>
+            {product.map((item, index) => (
+               <div
+                  key={nanoid()}
+                  className="flex flex-col break-inside-avoid-column"
+               >
+                  <div
+                     onClick={(event) => clickHandler(event, index)}
+                     className="relative cursor-pointer"
+                  >
+                     <img
+                        src={require(`../../dammyDB/${item.videoImg}`)}
+                        alt={item.videoImg}
+                        className="w-full 2xl:h-[300px] xl:h-[250px] lg:h-[200px] sm:h-[200px] object-contein z-50"
+                     />
+                     <div className="absolute top-2/4 left-2/4 -translate-x-2/4 -translate-y-2/4 ">
+                        <PlayButton />
                      </div>
-                  );
-               })}
+                  </div>
+                  <Typography className="uppercase font-normal lg:text-1xl border-b-[1px] border-black mt-2 mb-5">
+                     {item.description}
+                  </Typography>
+               </div>
+            ))}
          </div>
-
          <div
             className={`${
                openVideo ? 'scale-1 opacity-1 flex' : 'scale-0 opacity-0 '
@@ -96,12 +82,12 @@ const VideoSection = ({ product, videoSectionhandler }: videoSectionProps) => {
                   className={`${openVideo ? '' : 'hidden'} relative`}
                   width="100%"
                   height={`${window.innerWidth < 650 ? '50%' : '80%'}`}
-                  src={`${video[indexVideo].video}`}
-                  title={`${video[indexVideo].video}`}
+                  src={product[indexVideo].video}
+                  title={product[indexVideo].video}
                />
             </div>
             <CustomButtonExit
-               onClick={() => clickHandler()}
+               onClick={() => setOpenVideo(false)}
                buttonStyle="top-5 right-5 cursor-pointer z-30 "
             />
          </div>
