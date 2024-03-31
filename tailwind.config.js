4a11ef84b758ebddcd948c0b16239ad6a5b7f1a7
oleg <olegvraby@gmail.com>
2024-03-31 15:04:32 +0200

    finish

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,48 +1,48 @@
 import withMT from '@material-tailwind/react/utils/withMT';
 export default withMT({
-  content: [
-    './index.html',
-    './src/**/*.{js,ts,jsx,tsx}',
-    './node_modules/tw-elements/dist/js/**/*.js',
-  ],
+   content: [
+      './index.html',
+      './src/**/*.{js,ts,jsx,tsx}',
+      './node_modules/tw-elements/dist/js/**/*.js',
+   ],
 
-  theme: {
-    extend: {
-      animation: {
-        pulse: 'pulse 1.5s linear infinite',
+   theme: {
+      extend: {
+         animation: {
+            pulse: 'pulse 1.5s linear infinite',
+         },
+         keyframes: {
+            pulse: {
+               '0%': {
+                  transform: 'scale(0)',
+                  opacity: 0,
+               },
+               '75%': {
+                  transform: 'scale(3.5)',
+                  opacity: 1,
+               },
+               '100%': {
+                  transform: 'scale(4)',
+                  opacity: 0.5,
+               },
+            },
+         },
+         content: {
+            link: "url('/src/pages/img/arrow.svg')",
+         },
+         screens: {
+            sm: '640px',
+
+            md: '768px',
+
+            lg: '1024px',
+
+            xl: '1280px',
+
+            '2xl': '1536px',
+         },
       },
-      keyframes: {
-        pulse: {
-          '0%': {
-            transform: 'scale(0)',
-            opacity: 0,
-          },
-          '75%': {
-            transform: 'scale(3.5)',
-            opacity: 1,
-          },
-          '100%': {
-            transform: 'scale(4)',
-            opacity: 0.5,
-          },
-        },
-      },
-      content: {
-        link: "url('C:/Users/Oleg/Desktop/projects/react/02.tourist-shop/src/pages/img/arrow.svg')",
-      },
-      screens: {
-        sm: '640px',
-
-        md: '768px',
-
-        lg: '1024px',
-
-        xl: '1280px',
-
-        '2xl': '1536px',
-      },
-    },
-  },
-  darkMode: 'class',
-  plugins: [require('tw-elements/dist/plugin.cjs')],
+   },
+   darkMode: 'class',
+   plugins: [require('tw-elements/dist/plugin.cjs')],
 });
